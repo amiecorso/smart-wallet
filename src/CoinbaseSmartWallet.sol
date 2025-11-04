2b2987d798ccb6ab66cd8dd7aa2dafc095d5dfa0
Amie Corso <amie.corso@coinbase.com>
2025-11-04 08:15:54 -0800

    add test to prove isValidSignature passes for invalid sigs w sim path

--- src/CoinbaseSmartWallet.sol
+++ src/CoinbaseSmartWallet.sol
@@ -338,7 +338,7 @@ contract CoinbaseSmartWallet is ERC1271, IAccount, MultiOwnable, UUPSUpgradeable
 
             WebAuthn.WebAuthnAuth memory auth = abi.decode(sigWrapper.signatureData, (WebAuthn.WebAuthnAuth));
 
-            return WebAuthn.verifySim({challenge: abi.encode(hash), requireUV: false, webAuthnAuth: auth, x: x, y: y});
+            return WebAuthn.verifySim(abi.encode(hash), false, auth, x, y);
         }
 
         revert InvalidOwnerBytesLength(ownerBytes);
